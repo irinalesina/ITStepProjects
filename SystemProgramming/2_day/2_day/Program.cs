f78a7cb8213a226728ae3e734a0123264f318402
unknown <admin@A2-5.itstep.lan>
2016-02-23 21:32:50 +0300

    TODO

---
+++ SystemProgramming/2_day/2_day/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace _2_day
+{
+    class Program
+    {
+        static int n = 0;
+        static int threadName = 1;
+    
+        public static void Start()
+        {
+            Thread thread = new Thread(Func);
+            thread.Name = (threadName++).ToString();
+            thread.Start();
+        }
+
+        static void Func()
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                n++;
+            }
+            Console.WriteLine("{0}: {1}", Thread.CurrentThread.Name, n);
+        }
+
+        static void Main(string[] args)
+        {
+            Program.Start();
+            Program.Start();
+            Console.WriteLine("Main{0}", Program.n);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SystemProgramming/2_day/CopyFileData/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CopyFileData
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSource_Click(object sender, RoutedEventArgs e)
+        {
+            textBoxSource.Text = this.GetFilePath();
+        }
+
+        private string GetFilePath()
+        {
+            OpenFileDialog checkFile = new OpenFileDialog();
+            checkFile.InitialDirectory = "c:\\";
+
+            if (checkFile.ShowDialog() == null)
+                return "";
+
+            return checkFile.FileName;
+        }
+
+        private void buttonDestination_Click(object sender, RoutedEventArgs e)
+        {
+            textBoxDestination.Text = this.GetFilePath();
+        }
+
+        private void buttonStart_Click(object sender, RoutedEventArgs e)
+        {
+            Thread threadCopy = new Thread(CopyFile);
+        }
+
+        static void CopyFile(object src_, object dest_)
+        {
+            string src = src_.ToString();
+            string dest = dest_.ToString();
+            Stopwatch swTotal = Stopwatch.StartNew();
+            Stopwatch swRead = new Stopwatch();
+            Stopwatch swWrite = new Stopwatch();
+            int numReads = 0;
+            int numWrites = 0;
+            using (var outputFile = File.Create(dest))
+            {
+                using (var inputFile = File.OpenRead(src))
+                {
+                    int CopyBufferSize = 1000;
+                    var buffer = new byte[CopyBufferSize];
+                    int bytesRead;
+                    do
+                    {
+                        swRead.Start();
+                        bytesRead = inputFile.Read(buffer, 0, CopyBufferSize);
+                        swRead.Stop();
+                        ++numReads;
+                        if (bytesRead != 0)
+                        {
+                            swWrite.Start();
+                            outputFile.Write(buffer, 0, bytesRead);
+                            swWrite.Stop();
+                            ++numWrites;
+                        }
+                    } while (bytesRead != 0);
+                }
+            }
+            swTotal.Stop();
+        }
+    }
+}
