68b381c20234c5c382e73e79728f17e120d483a3
unknown <admin@A2-5.itstep.lan>
2016-02-25 20:50:37 +0300

    TODO

---
+++ Arr/Arr/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = { { 2, 4, 6, 8},
+                              { 2, 4, 6, 8},
+                              { 2, 4, 6, 8},
+                              { 2, 4, 6, 8} };
+            int a = matrix.GetLength(0);
+            int b = matrix.Length;
+        }
+    }
+}
---
+++ SystemProgramming/3_day/ProcessesAndThreads/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using System.Threading;
+using System.Collections.ObjectModel;
+
+namespace ProcessesAndThreads
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //Process[] processes = Process.GetProcesses();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            FillAllProcesses();
+            //Thread thteadUpdateProcessesList = new Thread(FillAllProcesses);
+            //thteadUpdateProcessesList.Start();
+        }
+
+        public void FillAllProcesses()
+        {
+            //foreach (var proc in Process.GetProcesses())
+            //{
+            //    ListViewItem item = new ListViewItem();
+            //    item.Content = proc.ProcessName;
+            //    item.Tag = proc.Id;
+            //    lvAllProcesses.Items.Add(item);
+            //}
+            lvAllProcesses.ItemsSource = (from pc in Process.GetProcesses() select new ListViewItem() { Content = pc.ProcessName, Tag = pc.Id }).ToList();
+        }
+
+        private void lvAllProcesses_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                lvAllProcessThreads.Items.Clear();
+                var item = (ListViewItem)lvAllProcesses.SelectedItem;
+                int procId = (int)item.Tag;
+                foreach (var t in Process.GetProcessById(procId).Threads)
+                {
+                    ListViewItem i = new ListViewItem();
+                    ProcessThread th = (ProcessThread)t;
+                    i.Content = th.Id;
+                    lvAllProcessThreads.Items.Add(i);
+                }
+ 
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
